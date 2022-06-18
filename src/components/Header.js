4b70a96d6fcc6a376d3806573dca44642f40c0d1
Kris Mally <kristophermally@gmail.com>
2022-06-18 13:09:15 -0600

    Display logo and user profile links

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,12 @@
 // This will be our logo link that will take us to the about page and our user button
 
 const Header = (props) => {
-    return <h1>Header</h1>
+    return (
+        <nav id="appInfo" className="appInfo">
+            <a href="#">PomoTom</a>
+            <a href="#">User</a>
+        </nav>
+    )
 }
 
 export default Header;
