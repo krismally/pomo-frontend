693ef949959303fc30378bf9f6521b38b7d01615
Jessie Zollinger <jzolly@hotmail.com>
2022-06-20 12:52:22 -0400

    setup basic routes in Main

--- src/components/Main.js
+++ src/components/Main.js
@@ -5,11 +5,33 @@ import { useState, useEffect} from 'react';
 import { Route } from 'react-router-dom';
 
 // Page Components
+import About from '../pages/About';
+import Config from '../pages/Config';
+import Lists from '../pages/Lists';
+import Timer from '../pages/Timer';
+import User from '../pages/User';
+
+
 
 const Main = (props) => {
     // State, logic, crud functions...
     return (
         <main>
+            <Route path="/about">
+                <About />
+            </Route>
+            <Route path="/user">
+                <User />
+            </Route>
+            <Route exact path="/">
+                <Timer />
+            </Route>
+            <Route path="/lists">
+                <Lists />
+            </Route>
+            <Route path="/config">
+                <Config />
+            </Route>
         </main>
     );
 }
