47c6e204055c1db8c2d9d76c584d914f9b3a3ce6
Kris Mally <kristophermally@gmail.com>
2022-06-20 20:56:37 -0600

    Display static timer progress bar

--- src/components/MainTimer.js
+++ src/components/MainTimer.js
@@ -1,13 +1,21 @@
+import ProgressBar from "react-customizable-progressbar"
+
 const MainTimer = (props) => {
     return (
         <div className="Timer">
-            <div className="TimerFace">
-                <h4>Session 4/5</h4>
-                <h1>25:00</h1>
-            </div>
+            <ProgressBar 
+                progress={100}
+                radius={100}
+                counterClockwise
+            >
+                <div className="indicator">
+                    <h1>25:00</h1>
+                    <h4>Session 4/5</h4>
+                    <button className="sound">Mute/Unmute</button>
+                    <button className="play-pause">Pause</button> 
+                </div>
+            </ProgressBar>
             <div className="controls">
-                <button className="sound">Mute/Unmute</button>
-                <button className="play-pause">Pause</button> 
             </div>
         </div>
     );
