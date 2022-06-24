5d78e298f6b469054a68ab72bd75bb1d6753e93f
Kris Mally <kristophermally@gmail.com>
2022-06-23 21:21:07 -0600

    Add audio component for future enhancement

---
+++ src/components/Alert.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Alert = () => {
+    return (
+        
+        <audio className="audio_tag" autoPlay >
+            <source src="../assets/timerDone.mp3" />
+        </audio>
+    )
+}
+
+export default Alert;
