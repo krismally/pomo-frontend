63d66af5db8c12f5bcfaf9a6527f11ee439730f9
Kris Mally <kristophermally@gmail.com>
2022-06-24 09:00:41 -0600

    Add logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,18 +1,28 @@
 // This will be our logo link that will take us to the about page and our user button
 import { Link as RouterLink } from "react-router-dom";
-import { createTheme, ThemeProvider } from "@mui/system";
-import { AppBar, Link, Avatar, Typography, Toolbar } from "@mui/material";
+import { Box, createTheme, ThemeProvider } from "@mui/system";
+import { AppBar, Link, Avatar, Typography, Toolbar, Grid } from "@mui/material";
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 import theme from "../Theme";
+import PomoTom from '../assets/PomoTom.png'
 
 const Header = (props) => {
     return (
-            <AppBar position="static">
+            <AppBar position="static" >
             <Toolbar>
-                <Typography sx={{ flexGrow: 1 }} >PomoTom</Typography>
-                <Avatar component={RouterLink} to="/user">
-                    <AccountCircleIcon color={theme.palette.text.primary}/>
+            <Box 
+            component="img"
+            alt="PomoTom Logo"
+            sx={{ ml: 2 }}
+            src={PomoTom}
+            position="fixed"
+            left={0}
+            />
+            <Box position={'fixed'} right={0}>
+                <Avatar sx={{mr: 2}} component={RouterLink} to="/user">
+                    <AccountCircleIcon position="fixed" right={0} color={theme.palette.text.primary}/>
                 </Avatar>
+            </Box>
             </Toolbar>
             </AppBar>
     )
